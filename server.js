bf9fdaef7a94a4cc174b0875c19c0b07cb24bcfa
khokharm <khokhar.mahad@hotmail.com>
2019-02-10 00:54:08 -0500

    Update server.js
    Updated file

--- server.js
+++ server.js
@@ -1,48 +1,33 @@
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const API_KEY = 'YOUR_API_KEY_HERE';
+"use strict";
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require("express");
+const bodyParser = require("npm install body-parser --save");
 
-const server = express();
-server.use(bodyParser.urlencoded({
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 
-server.use(bodyParser.json());
+restService.use(bodyParser.json());
 
-server.post('/get-movie-details', (req, res) => {
-
-    const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
-    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
-    http.get(reqUrl, (responseFromAPI) => {
-        let completeResponse = '';
-        responseFromAPI.on('data', (chunk) => {
-            completeResponse += chunk;
-        });
-        responseFromAPI.on('end', () => {
-            const movie = JSON.parse(completeResponse);
-            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
-
-            return res.json({
-                speech: dataToSend,
-                displayText: dataToSend,
-                source: 'get-movie-details'
-            });
-        });
-    }, (error) => {
-        return res.json({
-            speech: 'Something went wrong!',
-            displayText: 'Something went wrong!',
-            source: 'get-movie-details'
-        });
-    });
+restService.post("/dataCreateAndAssignment", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters
+      ? req.body.result.parameters
+      : "Seems like some problem. Speak again.";
+      Console.log (req.body.result.parameters); 
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
 });
 
-
-server.listen((process.env.PORT || 8000), () => {
-    console.log("Server is up and running...");
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
 });
