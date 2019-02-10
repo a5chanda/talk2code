793451485cb7271643e1374f630cbb110b3ca503
Abhinav Chanda <abhinav.chanda99@gmail.com>
2019-02-10 11:14:16 -0500

    Fix Possible

--- server.js
+++ server.js
@@ -27,32 +27,31 @@ function dataCreationAndAssignment(phase, variableName, equalsToCreation) {
 
 // Conditions is assumed to be an array
 // list of variable names
-function ifstatement (conditions, variableNames) {
-    //console.log(conditions);
-    if (variableNames.length == 1){
-      return 'if ' + variableNames[0];
-    }
-    return 'if ' + variableNames[0] +' ' + conditions[0] + ' ' + variableNames [1]; 
-    
-  };
+function ifstatement(conditions, variableNames) {
+  //console.log(conditions);
+  if (variableNames.length == 1) {
+    return 'if ' + variableNames[0];
+  }
+  return 'if ' + variableNames[0] + ' ' + conditions[0] + ' ' + variableNames[1];
+
+};
 
 
 restService.post("/", function (req, res) {
   //program = program.concat ('\n' + req.body.queryResult.queryText);
-  
+
   var speech = req.body.queryResult && req.body.queryResult.parameters ?
     "" : "Seems like some problem. Speak again.";
   console.log(speech);
 
-  if (speech=== "Seems like some problem. Speak again.") {
+  if (speech === "Seems like some problem. Speak again.") {
     return res.json({
       fulfillmentText: speech,
       //displayText: speech,
       source: "talk2code"
     });
-  } 
-  else {
-      
+  } else {
+
     console.log("Intent:");
     console.log(req.body.queryResult.intent.displayName);
     //console.log();
@@ -63,39 +62,40 @@ restService.post("/", function (req, res) {
     let intent = req.body.queryResult.intent.displayName;
     var speech = req.body.queryResult.queryText;
 
-    if(intent === "Default Welcome Intent"){
-        program = "";
-        return res.json({
-          });
+    if (intent === "Default Welcome Intent") {
+      program = "";
+      return res.json({});
     }
-    
+
     //create data assignment
     if (intent === "dataCreateAndAssignment") {
       console.log("dataCreation");
       program = program + ' \n ' + dataCreationAndAssignment(req.body.queryResult.queryText, req.body.queryResult.parameters.variableName, req.body.queryResult.parameters.equalsTo_creation);
-    } 
+    }
 
     //if statement
     else if (intent === "if statement") {
       console.log("inside if statement:");
       program = program + '\n' + ifstatement(req.body.queryResult.parameters.conditionals, req.body.queryResult.parameters.variableNames);
       console.log(program);
-    } 
+    }
 
     //arithmetic
     else if (intent === "arithmetic") {
       console.log("arithmetic");
-    } 
-    
+    }
+
     //end
     else if (intent === "compile") {
       console.log("End");
       console.log(program);
       //return program to the dialog flow
       return res.json({
-        fulfillmentText: program,
-        //displayText: speech,
-        source: "talk2code"
+        fulfillmentText: "displayed response",
+        fulfillmentMessages: [{
+          text: program,
+        }],
+        source: "talk2code",
       });
     }
 
@@ -107,7 +107,6 @@ restService.post("/", function (req, res) {
       source: "talk2code"
     });
   }
-
 
 });
 
