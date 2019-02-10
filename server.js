ac68059fa198804fad31571e07daf361a8ffaaa1
Abhinav Chanda <abhinav.chanda99@gmail.com>
2019-02-10 10:12:22 -0500

    Auto stash before merge of "master" and "origin/master"

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const restService = express();
 
 var program = "";
+
+
 
 restService.use(
   bodyParser.urlencoded({
@@ -15,11 +16,13 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-function dataCreationAndAssignment (phase, variableName, equalsToCreation ) {
+
+
+function dataCreationAndAssignment(phase, variableName, equalsToCreation) {
   if (equalsToCreation != null) {
-    return 'var ' + variableName + ' = '  + phase.split (equalsToCreation).pop() + ';'; 
+    return 'var ' + variableName + ' = ' + phase.split(equalsToCreation).pop() + ';';
   }
-  return 'var ' + variableName + ' ;' 
+  return 'var ' + variableName + ' ;';
 };
 
 
@@ -37,12 +40,21 @@ function ifstatement (conditions, variablesNames ) {
 restService.post("/", function(req, res) {
   program = program.concat ('\n' + req.body.queryResult.queryText);
 
-  console.log(req.body.queryResult.queryText);
-  console.log("Intent");
-  console.log(req.body.queryResult.intent.displayName);
-  console.log("prog")
-  console.log(program);
-  let intent = req.body.queryResult.intent.displayName
+  if (speech=== "Seems like some problem. Speak again.") {
+    return res.json({
+      fulfillmentText: speech,
+      //displayText: speech,
+      source: "talk2code"
+    });
+  } 
+  else {
+      
+    console.log("Intent:");
+    console.log(req.body.queryResult.intent.displayName);
+    //console.log();
+    console.log("Query:");
+    console.log(req.body.queryResult.queryText);
+    console.log();
 
   if(intent === "dataCreationAndAssignment"){
     console.log("dataCreation");
@@ -55,33 +67,52 @@ restService.post("/", function(req, res) {
   else if(intent === "End"){
     console.log("End");
     
-    //return program to the dialog flow
+    //create data assignment
+    if (intent === "dataCreateAndAssignment") {
+      console.log("dataCreation");
+      program = program + '\n' + dataCreationAndAssignment(req.body.queryResult.queryText, req.body.queryResult.parameters.variableName, req.body.queryResult.parameters.equalsTo_creation);
+    } 
+
+    //if statement
+    else if (intent === "if statement") {
+      console.log("inside if statement:");
+    } 
+
+    //arithmetic
+    else if (intent === "arithmetic") {
+      console.log("arithmetic");
+    } 
+    
+    //end
+    else if (intent === "End") {
+      console.log("End");
+      //return program to the dialog flow
+      return res.json({
+        fulfillmentText: speech,
+        //displayText: speech,
+        source: "talk2code"
+      });
+    }
+
+    //console.log (req.body.queryResult.parameters);
+    //console.log("here"); 
     return res.json({
-      fulfillmentText: program,
+
+      fulfillmentText: speech,
       //displayText: speech,
       source: "talk2code"
     });
   }
 
 
-  var program = req.body.queryResult && req.body.queryResult.parameters ? 
-              req.body.queryResult.queryText: "Seems like some problem. Speak again.";
-    //console.log (req.body.queryResult.parameters);
-    //console.log("here"); 
-  return res.json({
-    
-    fulfillmentText: speech,
-    //displayText: speech,
-    source: "talk2code"
-  });
 });
 
 
-restService.get("/", function(req, res) {
+restService.get("/", function (req, res) {
   res.send(program);
-  return res;  
+  return res;
 });
 
-restService.listen(process.env.PORT || 8000, function() {
+restService.listen(process.env.PORT || 8000, function () {
   console.log("Server up and listening");
 });
